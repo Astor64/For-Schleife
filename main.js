66d138a5cc5a65fd9598502a109b67fc7a14748f
Wolfgang Huesgen <wolfgang.huesgen@web.de>
2025-04-28 14:40:24 +0200

    second main

--- main.js
+++ main.js
@@ -1,4 +1,14 @@
 "use strict";
 
 //Loops For-Schleife
-function main() {}
+function main() {
+  // Loop
+
+  let names = ["Peter", "Jannick", "Maria"];
+  // Die Schleife läuft solange, bis die Bedingung unwahr wird.
+  for (let i = 0; i < names.length; i++) {
+    console.log(names.length);
+    console.log(i);
+    console.log("Index:  " + i + "  Name: " + names[i]);
+  }
+}
